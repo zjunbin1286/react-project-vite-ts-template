bdb520856369f91422dcac26adccd8b81ad6b5c3
CoderBin <1669940775@qq.com>
2024-03-12 21:07:46 +0800

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Project Vite Ts Templat
+
+🐟 react 项目模板，集成了常用编程规范工具链以网络请求等等
+
+> 注：可切换分支来使用对应功能
+
+持续更新中~
---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React + TS</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        🐟 基于 Vite 搭建的 react 项目模板，集成了 Axios、Eslint、Prettier、Huksy、Commit 等等，持续更新中~
+      </p>
+    </>
+  )
+}
+
+export default App
